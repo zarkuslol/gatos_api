d6f8970c1afb73c6c0a2e69f73000474e4691804
zarkuslol <lucasferlima8@gmail.com>
2023-08-22 10:48:39 -0300

    Controller Ping adicionado e configurado

---
+++ src/main/java/com/example/gatosapi/controllers/PingController.java
@@ -0,0 +1,15 @@
+package com.example.gatosapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+    @GetMapping
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("Olá, mundo");
+    }
+}
